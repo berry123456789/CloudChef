161f4b1cbba94e672f84de7565bed0a214342c46
berry123456789 <160743994+berry123456789@users.noreply.github.com>
2026-01-09 04:51:50 +0000

    Update main.jsx

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,17 +1,17 @@
+// web/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-
 import { AuthProvider } from "./auth/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
         <App />
-      </BrowserRouter>
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
