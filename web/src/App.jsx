0680858c31badcbb9e3c17ceb49360b4b6e32ac7
berry123456789 <160743994+berry123456789@users.noreply.github.com>
2026-01-09 02:05:51 +0000

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,5 @@
 // web/src/App.jsx
 import { Routes, Route, Navigate } from "react-router-dom";
-
 import Layout from "./components/Layout.jsx";
 
 import Recipes from "./pages/Recipes.jsx";
@@ -8,6 +7,9 @@ import Recipe from "./pages/Recipe.jsx";
 import Create from "./pages/Create.jsx";
 import Edit from "./pages/Edit.jsx";
 import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+
+import RequireAuth from "./auth/RequireAuth.jsx";
 
 export default function App() {
   return (
@@ -15,9 +17,29 @@ export default function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Recipes />} />
         <Route path="/recipes/:id" element={<Recipe />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/edit/:id" element={<Edit />} />
+
+        {/* protected routes */}
+        <Route
+          path="/create"
+          element={
+            <RequireAuth>
+              <Create />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/edit/:id"
+          element={
+            <RequireAuth>
+              <Edit />
+            </RequireAuth>
+          }
+        />
+
+        {/* auth routes */}
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
