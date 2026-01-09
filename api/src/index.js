13853d6cd178381b97385d8489eb63cf49fa6b75
berry123456789 <160743994+berry123456789@users.noreply.github.com>
2026-01-09 01:10:56 +0000

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,11 @@
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
 
-require('./functions/CreateRecipe');
-require('./functions/GetRecipe');
-require('./functions/UpdateRecipe');
-require('./functions/DeleteRecipe');
-require('./functions/UploadRecipeImage');
-require('./functions/ListRecipes');
-require('./functions/RegisterUser');
-require('./functions/LoginUser');
+require("./functions/CreateRecipe");
+require("./functions/GetRecipe");
+require("./functions/UpdateRecipe");
+require("./functions/DeleteRecipe");
+require("./functions/UploadRecipeImage");
+require("./functions/ListRecipes");
 
+require("./functions/RegisterUser");
+require("./functions/LoginUser");
